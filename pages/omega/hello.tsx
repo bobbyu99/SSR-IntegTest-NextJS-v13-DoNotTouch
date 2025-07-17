0147bd81fd81914d27096b469accdc51ec6331a8
Bobby Yu <bobbyu@amazon.com>
2025-07-17 10:37:04 -0700

    add getServerSideProps

--- pages/omega/hello.tsx
+++ pages/omega/hello.tsx
@@ -1,5 +1,22 @@
 // pages/omega/hello.tsx
 import { NextPage } from 'next'
+import type { GetServerSidePropsContext } from 'next'
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const { req } = context; // Access the incoming request object
+
+  // Access specific headers using req.headers
+  const identity = (req.headers['x-omega-authn-identity'] as string) || '<<missing>>'; // Identity token from Compute Origin
+
+  console.log('🔥 SSR - propagated identity token:', identity);
+
+  // You can then use these headers in your page logic or pass them as props
+  return {
+    props: {
+      identity,
+    },
+  };
+}; 
 
 const OmegaHello: NextPage = () => (
   <div style={{ fontFamily: 'system-ui, sans-serif', padding: 20 }}>
